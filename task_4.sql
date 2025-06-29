047e4c95510e8b996e7794c137abc7cb253bdade
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-06-29 21:45:26 +0100

    Modified

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,11 @@
-USE alx_book_store;
 
-SELECT * FROM books;
+
+SELECT 
+    
+    COLUMN_NAME, 
+    COLUMN_TYPE 
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = "alx_book_store" 
+    AND TABLE_NAME = 'books';
